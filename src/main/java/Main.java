f4327212a2e21f3bc6f4f3ca88f3675b1db6bb90
Шундеев Николай Николаевич <NNShundeev@sberbank.ru>
2021-06-21 19:58:59 +0300

    two feature/1 commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("dev");
         System.out.println("feature/1");
+        System.out.println("two feature/1");
     }
 }
